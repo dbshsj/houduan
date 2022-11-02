186a61fe1517da82a6b72d0eb92e88a8d57a9f44
dbshsj <105916065+dbshsj@users.noreply.github.com>
2022-11-02 22:47:15 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express');
+//引入模块
+import express from "express"
+import {userRouter} from "./users/user.js"
 
 const app = express();
+app.use('/api/users',userRouter)
+
 
 app.listen(3007, () => {
     console.log('software running at http://127.0.0.1:3007');
