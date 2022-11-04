6909150f0cabf8a3e4d466929a4e52c14d66a081
dbshsj <105916065+dbshsj@users.noreply.github.com>
2022-11-04 20:48:30 +0800

    Add files via upload

--- posts.js
+++ posts.js
@@ -1,6 +1,28 @@
+//数据库操作模块
 //引入db模块，数据库连接
 import {db} from "./utils/db.js"
 
-async function createUser(){
-   
+//用户注册
+export async function createUser(userData){
+   try{
+        let s1 = await db.user.create({
+        data:{
+            name:"1",
+            phone:userData.phone,
+            email:userData.email,
+            password:userData.password
+        }
+       });
+       const s2 = {
+            code:"200",
+            message:"注册成功！",
+            data:s1
+       }
+        return new Promise((resolve,reject)=>{
+            resolve(s2);
+        })
+   }catch(err){
+        console.log(err);
+
+   }
 }
--- users/user.js
+++ users/user.js
@@ -1,11 +1,13 @@
 //导入路由模块
 import express from "express"
+//导入数据库处理函数
+import { createUser } from "../posts.js"
 
 //创建路由实例对象
 const userRouter = express.Router()
 
 userRouter.get('/:id',(req,res)=>{
-    res.send(req.params.id)
+    
 })
 
 
@@ -13,8 +15,12 @@ userRouter.post('/login',(req,res)=>{
     
 })
 
+
 userRouter.post('/regist',(req,res)=>{
-    
+    const s1 = createUser(req.body)
+    s1.then((value)=>{
+        res.send(value)
+    })
 })
 
 userRouter.post('/find',(req,res)=>{
