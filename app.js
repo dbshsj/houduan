585cf9500d6c7a2f625690d27ad318fee4ee6204
dbshsj <105916065+dbshsj@users.noreply.github.com>
2022-11-04 20:49:23 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ import express from "express"
 import {userRouter} from "./users/user.js"
 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
 app.use('/api/users',userRouter)
 
 
-app.listen(3007, () => {
-    console.log('software running at http://127.0.0.1:3007');
+app.listen(80, () => {
+    console.log('software running at http://127.0.0.1');
 })
